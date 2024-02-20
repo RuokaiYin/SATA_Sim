c0e250b8128302774cc653a00af6444684b46811
Ruokai Yin <ryin25@wisc.edu>
2024-02-20 16:42:50 -0500

    update the computation instance

--- new_satasim/arithmetics.py
+++ new_satasim/arithmetics.py
@@ -1,46 +1,37 @@
 from compute_instance import Primitive_Computation_Unit
-from utils import oneD_linear_interpolation, oneD_quadratic_interpolation
-import yaml
+
 
 synthesis_file_location = './arith_32nm-2_5ns.yaml'
-
-
-
-def check_synthesis_file(unit, key):
-
-    global synthesis_file_location
-    
-    with open(synthesis_file_location, 'r') as file:
-        data = yaml.safe_load(file)
-    data = data.get(key,{})
-    unit.scaling = data['interpolation']
-    for instance in data['already_syn']:
-        if instance['bw'] == unit.bit_width: #* The case where the provided bit_width is already existed in the synthesis file
-            unit.info['area'] = instance['attributes']['area']
-            unit.info['power-dynamic'] = instance['attributes']['power-dynamic']
-            unit.info['power-leakage'] = instance['attributes']['power-leakage']
-            unit.info['access-latency'] = instance['attributes']['access-latency']
-            unit.info['cycle-time'] = instance['attributes']['cycle-time']
-        else: #* We scale the area and power with the interpolation.
-            known_dict = data['already_syn']
-            if unit.scaling == 'linear':
-                unit.info['area'] = oneD_linear_interpolation(unit.bit_width, 'area', known_dict)
-                unit.info['power-dynamic'] = oneD_linear_interpolation(unit.bit_width, 'power-dynamic', known_dict)
-                unit.info['power-leakage'] = oneD_linear_interpolation(unit.bit_width, 'power-leakage', known_dict)
-            else:
-                unit.info['area'] = oneD_quadratic_interpolation(unit.bit_width, 'area', known_dict)
-                unit.info['power-dynamic'] = oneD_quadratic_interpolation(unit.bit_width, 'power-dynamic', known_dict)
-                unit.info['power-leakage'] = oneD_quadratic_interpolation(unit.bit_width, 'power-leakage', known_dict)
-            unit.info['access-latency'] = instance['attributes']['access-latency']
-            unit.info['cycle-time'] = instance['attributes']['cycle-time']
-
 
 class Multiplier(Primitive_Computation_Unit):
     def __init__(self, name, bit_width):
         super().__init__(name, bit_width)
         self.name = name
         self.bit_width = bit_width
-        check_synthesis_file(self,'MUL')
+        self.check_synthesis_file('MUL',synthesis_file_location)
+
+
+class Accumulator(Primitive_Computation_Unit):
+    def __init__(self, name, bit_width):
+        super().__init__(name, bit_width)
+        self.name = name
+        self.bit_width = bit_width
+        self.check_synthesis_file('ACC',synthesis_file_location)
+
+class Adder(Primitive_Computation_Unit):
+    def __init__(self, name, bit_width):
+        super().__init__(name, bit_width)
+        self.name = name
+        self.bit_width = bit_width
+        self.check_synthesis_file('ADD',synthesis_file_location)
+
+
+class Register(Primitive_Computation_Unit):
+    def __init__(self, name, bit_width):
+        super().__init__(name, bit_width)
+        self.name = name
+        self.bit_width = bit_width
+        self.check_synthesis_file('REG',synthesis_file_location)
 
 
 def test():
--- new_satasim/compute_instance.py
+++ new_satasim/compute_instance.py
@@ -1,15 +1,17 @@
+from utils import oneD_linear_interpolation, oneD_quadratic_interpolation
+import yaml
 
 class Primitive_Computation_Unit:
-    def __init__(self, name, bit_width, 
-                 scaling = 'linear', info = {"area": 0,
-                                            "power-dynamic": 0,
-                                            "power-leakage": 0,
-                                            "access-latency": 0,
-                                            "cycle-time": 0}):
+    def __init__(self, name, bit_width, scaling = 'linear', info = {"area": 0,
+                                                            "power-dynamic": 0,
+                                                            "power-leakage": 0,
+                                                            "access-latency": 0,
+                                                            "cycle-time": 0}):
         self.name = name
         self.bit_width = bit_width
         self.info = info
         self.scaling = scaling
+        self.access_counts = 0
 
     def set_bit_width(self, bit_width):
         self.bit_width = bit_width
@@ -23,28 +25,87 @@ class Primitive_Computation_Unit:
     def get_area(self):
         return self.info["area"]
     
+    #*                               *#
+    #* This returns the power in pW. *#
+    #*                               *#
     def get_dpower(self):
         return self.info["power-dynamic"]
     
+    #*                               *#
+    #* This returns the power in pW. *#
+    #*                               *#
     def get_lpower(self):
         return self.info["power-leakage"]
 
-    #* The cycle time used in the synthesis. Cycles taken from input to output. *#
+    #*                                       *#
+    #* The cycle time used in the synthesis. *# 
+    #* Cycles taken from input to output.    *#
+    #*                                       *#
     def get_cycletime(self):
         return self.info["cycle-time"]
     
     #*                                  *#
     #* This returns the data in cycles. *#
     #*                                  *#
-    def get_latency(self):
+    def get_access_latency(self):
         return self.info["access-latency"]
-    
+
+    #*                                                                                                     *#
+    #* This function checks the provided synthesis file. If the stats already in the file, fill the stats. *#
+    #* Otherwise, interpolate the bitwidth of the units to get the desired attributes.                     *#
+    #*                                                                                                     *#
+    def check_synthesis_file(self, key, synthesis_file_location):
+        
+        with open(synthesis_file_location, 'r') as file:
+            data = yaml.safe_load(file)
+        data = data.get(key,{})
+        self.scaling = data['interpolation']
+        for instance in data['already_syn']:
+            if instance['bw'] == self.bit_width: #* The case where the provided bit_width is already existed in the synthesis file
+                self.info['area'] = instance['attributes']['area']
+                self.info['power-dynamic'] = instance['attributes']['power-dynamic']
+                self.info['power-leakage'] = instance['attributes']['power-leakage']
+                self.info['access-latency'] = instance['attributes']['access-latency']
+                self.info['cycle-time'] = instance['attributes']['cycle-time']
+            else: #* We scale the area and power with the interpolation.
+                known_dict = data['already_syn']
+                if self.scaling == 'linear':
+                    self.info['area'] = oneD_linear_interpolation(self.bit_width, 'area', known_dict)
+                    self.info['power-dynamic'] = oneD_linear_interpolation(self.bit_width, 'power-dynamic', known_dict)
+                    self.info['power-leakage'] = oneD_linear_interpolation(self.bit_width, 'power-leakage', known_dict)
+                else:
+                    self.info['area'] = oneD_quadratic_interpolation(self.bit_width, 'area', known_dict)
+                    self.info['power-dynamic'] = oneD_quadratic_interpolation(self.bit_width, 'power-dynamic', known_dict)
+                    self.info['power-leakage'] = oneD_quadratic_interpolation(self.bit_width, 'power-leakage', known_dict)
+                self.info['access-latency'] = instance['attributes']['access-latency']
+                self.info['cycle-time'] = instance['attributes']['cycle-time']
+
+
+    #*                                *#
+    #* This returns the energy in pJ. *#
+    #*                                *#
     def get_access_energy(self):
         return self.info["access-latency"] * self.info["cycle-time"] * self.info["power-dynamic"]
     
-    #! TODO: to implement this based on interpolation.
-    def scale_info(self, new_bw):
-        return 0
+
+    #*                                     *#
+    #* This returns the latency in cycles. *#
+    #*                                     *#
+    def get_latency(self):
+        return self.info["access-latency"] * self.access_counts
+    
+    #*                                 *#
+    #* This returns the # of accesses. *#
+    #*                                 *#
+    def get_accesses(self):
+        return self.access_counts
+    
+
+    #*                                 *#
+    #* This updates the # of accesses. *#
+    #*                                 *#
+    def update_accesses(self, accesses):
+        self.access_counts += accesses
 
 
 
