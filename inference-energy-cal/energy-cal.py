3715ee9bed08d63ad4093659799c7e70845ffe4b
Ruokai Yin <ruokai.yin@yale.edu>
2024-08-15 07:26:35 -0400

    modify fc bugs and add workloads for relevant works

--- inference-energy-cal/energy-cal.py
+++ inference-energy-cal/energy-cal.py
@@ -32,7 +32,7 @@ def read_cycles(cycle_filepath, arch_configpath, workload_filepath):
             cycle_stats['total_cycles'] += (stats.get('SRAM OFMAP Start Cycle') + stats.get('SRAM OFMAP Cycles'))*timestep + stats.get('SRAM Filter Cycles')
             cycle_stats['sram_i_reads'] += stats.get('SRAM IFMAP Reads')
             cycle_stats['sram_w_reads'] += stats.get('SRAM Filter Reads')
-            cycle_stats['sram_o_writes'] += stats.get('SRAM OFMAP Writes')
+            cycle_stats['sram_o_writes'] += stats.get('SRAM OFMAP Writes')*timestep
             cycle_stats['dram_i_reads'] += stats.get('DRAM IFMAP Reads')
             cycle_stats['dram_w_reads'] += stats.get('DRAM Filter Reads')
             cycle_stats['dram_o_writes'] += stats.get('DRAM OFMAP Writes')
@@ -67,10 +67,15 @@ def extract_workload(workload_filepath):
     for l in layers:
         # print(l)
         attr = l['attributes']
-        of_w = (math.floor((attr['IFMAP Width'] + 2 - attr['Filter Width'])/attr['Strides']) + 1) # TODO: Adding support for different padding, now assume 1
-        of_h = (math.floor((attr['IFMAP Height'] + 2 - attr['Filter Height'])/attr['Strides']) + 1)
-        total_lif += attr['Num Filter'] * (of_w * of_h) * timestep
-        total_mac += attr['Num Filter'] * (of_w * of_h) * (attr['Filter Width'] * attr['Filter Height'] * attr['Channels']) * timestep * (1-sparsity)
+        name = l['name']
+        if 'Conv' in name:
+            of_w = (math.floor((attr['IFMAP Width'] + 2 - attr['Filter Width'])/attr['Strides']) + 1) # TODO: Adding support for different padding, now assume 1
+            of_h = (math.floor((attr['IFMAP Height'] + 2 - attr['Filter Height'])/attr['Strides']) + 1)
+            total_lif += attr['Num Filter'] * (of_w * of_h) * timestep
+            total_mac += attr['Num Filter'] * (of_w * of_h) * (attr['Filter Width'] * attr['Filter Height'] * attr['Channels']) * timestep * (1-sparsity)
+        elif 'FC' in name: #! Added support for FC.
+            total_mac += attr['Num Filter'] * attr['Channels'] * timestep * (1-sparsity)
+            total_lif += attr['Num Filter'] * timestep
     
     workload_dic['total_mac'] = int(total_mac)
     workload_dic['total_lif'] = int(total_lif)
@@ -126,6 +131,9 @@ def comp_computation_energy(comp_filepath, cycle_dict, arch_configpath, workload
         total_comp += subtotal
     comp_dic['total'] = total_comp
 
+    comp_dic['total_mac_ops'] = workload_dic['total_mac']
+    comp_dic['total_activation_ops'] = workload_dic['total_lif']
+
     file_path = './results/computation-energy.yaml'
     # print()
     with open(file_path, 'w') as yaml_file:
@@ -140,9 +148,12 @@ def comp_mem_energy(mem_filepath, cycle_dict, arch_configpath):
     arch = arch_data.get('architecture')
     freq = arch.get('clock-frequency') * 1000000 #! Need to convert to MHz first, in yaml, M is not take into consideration
     cyc = 1/freq
-
+    x_bw = arch.get('act-prec')
+    w_bw = arch.get('weight-prec')
+    o_bw = arch.get('output-prec')
     convert_ratio = 1000000 #! Need again to convert SRAM leakage to nJ, the leaking power is in mW from the mem-stats.yaml
     # timestep = arch.get('timestep')
+    
 
     with open(mem_filepath, 'r') as file:
         data = yaml.safe_load(file)
@@ -154,13 +165,13 @@ def comp_mem_energy(mem_filepath, cycle_dict, arch_configpath):
         sub_total = 0.0
         if name not in mem_dic:
             mem_dic[name] = {}
-        mem_dic[name]['ifmap'] = entry.get('read energy') * cycle_dict['dram_i_reads'] # ? CHECK: is the energy in nJ ?
+        mem_dic[name]['ifmap'] = entry.get('read energy') * (x_bw/8) * cycle_dict['dram_i_reads'] # ? CHECK: is the energy in nJ ?
         total_dram += mem_dic[name]['ifmap']
         sub_total  += mem_dic[name]['ifmap']
-        mem_dic[name]['weight'] = entry.get('read energy') * cycle_dict['dram_w_reads'] # ? CHECK: is the energy in nJ ?
+        mem_dic[name]['weight'] = entry.get('read energy') * (w_bw/8) * cycle_dict['dram_w_reads'] # ? CHECK: is the energy in nJ ?
         total_dram += mem_dic[name]['weight']
         sub_total  += mem_dic[name]['weight']
-        mem_dic[name]['ofmap'] = entry.get('read energy') * cycle_dict['dram_o_writes'] # ? CHECK: is the energy in nJ ?
+        mem_dic[name]['ofmap'] = entry.get('read energy') * (o_bw/8) * cycle_dict['dram_o_writes'] # ? CHECK: is the energy in nJ ?
         total_dram += mem_dic[name]['ofmap']
         sub_total  += mem_dic[name]['ofmap']
         mem_dic[name]['total'] = sub_total
@@ -173,21 +184,23 @@ def comp_mem_energy(mem_filepath, cycle_dict, arch_configpath):
         if name not in mem_dic:
             mem_dic[name] = {}
         if 'ifmap' in name:
-            mem_dic[name]['ifmap-dynamic'] = entry.get('read dynamic energy') * cycle_dict['sram_i_reads'] # ? CHECK: is the energy in nJ ?
+            mem_dic[name]['ifmap-dynamic'] = entry.get('read dynamic energy') * (x_bw/8) * cycle_dict['sram_i_reads'] # ? CHECK: is the energy in nJ ?
             mem_dic[name]['ifmap-leakage'] = entry.get('leakage power') * cycle_dict['total_cycles'] * cyc * convert_ratio #! The power is in mW, need to scale it to nJ
             mem_dic[name]['ifmap-total']= mem_dic[name]['ifmap-dynamic'] + mem_dic[name]['ifmap-leakage']
             total_sram += mem_dic[name]['ifmap-dynamic'] + mem_dic[name]['ifmap-leakage']
         elif 'weight' in name:
-            mem_dic[name]['weight-dynamic'] = entry.get('read dynamic energy') * cycle_dict['sram_w_reads'] # ? CHECK: is the energy in nJ ?
+            mem_dic[name]['weight-dynamic'] = entry.get('read dynamic energy') * (w_bw/8) * cycle_dict['sram_w_reads'] # ? CHECK: is the energy in nJ ?
             mem_dic[name]['weight-leakage'] = entry.get('leakage power') * cycle_dict['total_cycles'] * cyc * convert_ratio
             mem_dic[name]['weight-total']= mem_dic[name]['weight-dynamic'] + mem_dic[name]['weight-leakage']
             total_sram += mem_dic[name]['weight-dynamic'] + mem_dic[name]['weight-leakage']
         elif 'ofmap' in name:
-            mem_dic[name]['ofmap-dynamic'] = entry.get('write dynamic energy') * cycle_dict['sram_o_writes'] # ? CHECK: is the energy in nJ ?
+            mem_dic[name]['ofmap-dynamic'] = entry.get('write dynamic energy') * (o_bw/8) * cycle_dict['sram_o_writes'] # ? CHECK: is the energy in nJ ?
             mem_dic[name]['ofmap-leakage'] = entry.get('leakage power') * cycle_dict['total_cycles'] * cyc * convert_ratio
             mem_dic[name]['ofmap-total']= mem_dic[name]['ofmap-dynamic'] + mem_dic[name]['ofmap-leakage']
             total_sram += mem_dic[name]['ofmap-dynamic'] + mem_dic[name]['ofmap-leakage']
     mem_dic['sram_total'] = total_sram
+
+    mem_dic['total'] = total_dram + total_sram
 
     # print(mem_dic)
     file_path = './results/memory-energy.yaml'
@@ -200,17 +213,14 @@ def comp_mem_energy(mem_filepath, cycle_dict, arch_configpath):
 if __name__ == "__main__":
 
     cycle_path = './results/cycle-stat.yaml'
-    arch_path = './sata-config.yaml'
+    arch_path = './arch-config.yaml'
     mem_path = './results/mem-stat.yaml'
     comp_path = './results/comp-stat.yaml'
     # work_path = './workloads/workload_direct.yaml'
     work_path = './workload.yaml'
 
     cycle_stat = read_cycles(cycle_path, arch_path, work_path)
-    # print(cycle_stat)
-    
     workload_dic = extract_workload(work_path)
-    # print(workload_dic)
     mem_dic = comp_mem_energy(mem_path, cycle_stat, arch_path)
     comp_dic = comp_computation_energy(comp_path, cycle_stat, arch_path, workload_dic)
     
